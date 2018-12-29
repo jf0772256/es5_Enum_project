6e909f4f584f8dbdbf771b2784a0bbc6c9a83f4c
Jesse Fender <jf0772256@otc.edu>
2018-12-28 20:20:17 -0600

    general data scaffolding
    some starter functions and the class definition function

---
+++ src/enum.js
@@ -0,0 +1,14 @@
+
+function ES5Enum(options){
+  //
+}
+
+var isArray=function(a){
+  return (!!a) && (a.constructor === Array);
+};
+var isObject=function(a){
+  return (!!a) && (a.constructor === Object);
+};
+var isNumber=function(a){
+  return (!!a) && (a.constructor === Number);
+}
