f80a436c06a58571208bfed0c1327f074405d73c
Jesse Fender <jf0772256@otc.edu>
2018-12-28 20:21:49 -0600

    added good practice spacing

--- src/enum.js
+++ src/enum.js
@@ -3,12 +3,12 @@ function ES5Enum(options){
   //
 }
 
-var isArray=function(a){
+var isArray = function(a) {
   return (!!a) && (a.constructor === Array);
 };
-var isObject=function(a){
+var isObject = function(a) {
   return (!!a) && (a.constructor === Object);
 };
-var isNumber=function(a){
+var isNumber = function(a) {
   return (!!a) && (a.constructor === Number);
 }
