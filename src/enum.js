faa8821ffe4cd52677d3d14d0cc0845792d74397
Jesse Fender <jf0772256@otc.edu>
2018-12-28 21:16:28 -0600

    Added comments to objJoin function

--- src/enum.js
+++ src/enum.js
@@ -19,6 +19,15 @@ var isNumber = function(a) {
   return (!!a) && (a.constructor === Number);
 }
 
+/**
+ * Function to combine two objects into one. Takes 3 arguments to work, fourth is optional for controlled joins. 
+ * As of yet doesnt support complex or deep checks not that it ever will.
+ * Make sure not to just pass {} to the output, the output shoudl be set to a variable so that you can use it later.
+ * @param {{}} output Required. This is the object that will be the result of the merged objects.
+ * @param {{}} defaultOptions Required. This is the programmer supplied options and values object.
+ * @param {{}} userSupplied Required. This is the object that the user supplies and the values that they chose.
+ * @param {boolean} ignoreExtraProperties Optional. This flag defaults to false if not set. If true, will ignore properties within the user supplied options if they are not also existing in the programmer supplied options.
+ */
 var objJoin = function (output, defaultOptions, userSupplied, ignoreExtraProperties){
   var isObj1 = isObject(defaultOptions),  
     isObj2 = isObject(userSupplied);
@@ -40,7 +49,6 @@ var objJoin = function (output, defaultOptions, userSupplied, ignoreExtraPropert
         output[e] = userSupplied[e];
       }
     }
-    return output;
   } else {
     throw new Error('Type Error: One or both object parameters were not objects! We were unable to proceed with the operation.');
   }
